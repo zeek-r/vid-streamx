c76d69ea60adbb0b2cb033b9ebbc82273630a14b
zeek-r <bistaroshan03@gmail.com>
2020-03-30 00:25:01 +0545

    Added index router

---
+++ src/api/http/routes/index.js
@@ -0,0 +1,22 @@
+function init(router, handlers) {
+  console.log("loading routes");
+  // Fill in routes, can multiplex to more files, if required
+  router
+    .route("/")
+    .get((req, res, next) => {
+      console.log(req);
+      res.json({ hello: "world" });
+    })
+    .all((req, res) => {
+      res.status(405).send({ message: "Not allowed" });
+    });
+
+  router.route("/stream/*").get(handlers.videoHandlers.getStream);
+
+  // 404 handlers, handle 405 in individual route like above
+  router.route("*").all((req, res, next) => {
+    res.status(404).json({ message: "Route not found" });
+  });
+}
+
+module.exports = { init };
