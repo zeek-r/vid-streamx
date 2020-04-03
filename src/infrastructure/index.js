51bdc8b6b278dee126031216ad2b4f32eab15f40
zeek-r <bistaroshan03@gmail.com>
2020-04-04 01:42:17 +0545

    exported redisInfra

--- src/infrastructure/index.js
+++ src/infrastructure/index.js
@@ -1,11 +1,15 @@
 const video = require("./video");
+const redis = require("./redis");
+const config = require("config");
 
 function init({ aws = {} }) {
   const s3 = new aws.S3();
   const videoInfra = video.init({ S3: s3 });
+  const redisInfra = redis.init();
 
   return {
-    videoInfra
+    videoInfra,
+    redisInfra
   };
 }
 
