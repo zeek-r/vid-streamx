635a8e7ca60e9dd97532a67afedd9345d9d469db
zeek-r <bistaroshan03@gmail.com>
2020-04-04 01:39:05 +0545

    Added port from config

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const { http } = require("./src/api");
 const services = require("./src/services");
+const config = require("./src/commons/config");
 
 const main = () => {
   const app = services.init();
   const { server } = http.init(app);
 
-  server.listen("3000", () => {
+  server.listen(config.apiPort, () => {
     console.log("started at 3000");
   });
 };
