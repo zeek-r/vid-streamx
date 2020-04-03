b7939b7a628073358c9d0b1648cebcb4ff6d0bae
zeek-r <bistaroshan03@gmail.com>
2020-04-04 01:39:44 +0545

    Injected auth handler

--- src/api/http/handlers/index.js
+++ src/api/http/handlers/index.js
@@ -1,10 +1,13 @@
 const video = require("./video");
+const auth = require("./auth");
 
 function init(app = {}) {
   const videoHandlers = video.init({ service: app.videoService });
+  const authHandlers = auth.init({ service: app.authService });
 
   return {
-    videoHandlers
+    videoHandlers,
+    authHandlers
   };
 }
 
