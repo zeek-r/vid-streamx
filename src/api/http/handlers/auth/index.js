b98fcff2d8c1f069e296d7bff541ba6ac65a6364
zeek-r <bistaroshan03@gmail.com>
2020-04-04 01:37:11 +0545

    Added auth handler

---
+++ src/api/http/handlers/auth/index.js
@@ -0,0 +1,23 @@
+let authService = null;
+
+async function authenticate(req, res, next) {
+  try {
+    const userID = req["headers"]["X-STREAM-TOKEN"];
+    const authenticated = await authService.authenticate(userID);
+
+    if (authenticated) {
+      return next();
+    }
+    return res.status(401).json({ message: "Unauthorized" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message || "Internal Server Error" });
+  }
+}
+
+function init({ service = {} }) {
+  authService = service;
+  return {
+    authenticate
+  }
+}
+module.exports = { init };
