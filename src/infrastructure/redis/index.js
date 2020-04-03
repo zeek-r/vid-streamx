8822b7872aef9c21c8c1c564f0c885ebf770a80a
zeek-r <bistaroshan03@gmail.com>
2020-04-04 01:38:11 +0545

    Added redis getObject

---
+++ src/infrastructure/redis/index.js
@@ -0,0 +1,47 @@
+const redis = require("redis");
+const config = require("../../commons/config");
+
+let conn = null;
+let retryCounter = 0;
+
+const getObject = async (key) => {
+  // return { "token": "test-token", userID: key }
+  return new Promise((resolve, reject) => {
+    conn.hmgetall(key, (err, value) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve(value);
+    })
+  })
+}
+
+
+function init() {
+  if (conn !== null) {
+    return conn;
+  }
+
+  conn = redis.createClient({
+    host: config.redisHost,
+    port: config.redisPort
+  });
+
+  conn.on("error", errorHandler);
+
+  conn.getObject = getObject;
+  return conn;
+}
+
+function errorHandler(error) {
+  console.error(error);
+  retryCounter++;
+  if(retryCounter === config.redisRetryCount) {
+    console.error(`retry counter exceeded: ${retryCounter}, exiting`);
+    process.exit(0);
+  }
+  init();
+  return;
+}
+
+module.exports = { init }
